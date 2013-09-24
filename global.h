cd109537d7313f4442499c4ac8f99ff9182cba0f
pdigiglio <p.digiglio91@gmail.com>
2013-09-24 14:55:46 +0200

    aggiorno global.h

--- global.h
+++ global.h
@@ -1,13 +1,15 @@
-/* Fille header contenente i parametri del sistema */
+/* File header contenente i parametri del sistema */
 
 #ifndef  global_INC
 #define  global_INC
 
-#define N 6	/* numero atomi asse x */
-#define M 6	/* numero atomi asse y */
+#define N 12	/* numero atomi asse x */
+#define M 12	/* numero atomi asse y */
 #define T 256	/* 4 * numero di Trotter */
 
 #define B 5
 #define J 1
+
+#define MSR 500000
 
 #endif   /* ----- #ifndef global_INC  ----- */
